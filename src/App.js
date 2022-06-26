dedb26c1c81ba8c3b8b49734f11e8a67f62a9626
erfan shafiee <erfan74sh@gmail.com>
2022-06-26 21:38:26 +0430

    wrap profile route to ProtctedRoute

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Profile from "./pages/Profile";
+import ProtectedRoute from "./routes/ProtectedRoute";
 
 import "./App.css";
 
@@ -13,7 +14,9 @@ function App() {
 			<Route path="/" element={<Home />} />
 			<Route path="/login" element={<Login />} />
 			<Route path="/register" element={<Register />} />
-			<Route path="/profile" element={<Profile />} />
+			<Route element={<ProtectedRoute />}>
+				<Route path="/profile" element={<Profile />} />
+			</Route>
 		</Routes>
 	);
 }
