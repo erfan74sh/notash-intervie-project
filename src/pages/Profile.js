6c3b54bfdf5da9cccf1fe3595d9a6f9720ce3671
erfan shafiee <erfan74sh@gmail.com>
2022-06-26 21:39:19 +0430

    create Profile page

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,7 +1,37 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import UserService from "../services/user.service";
 
 const Profile = () => {
-	return <div>Profile</div>;
+	const [user, setUser] = useState({
+		username: "",
+		email: "",
+		created: "",
+		updated: "",
+	});
+	useEffect(() => {
+		(async () => {
+			try {
+				const response = await UserService.getCurrentUser();
+				if (response.data && response.data.account) {
+					const { username, email, created, updated } = response.data.account;
+					setUser({ username, email, created, updated });
+				}
+			} catch (err) {
+				console.log(err);
+			}
+		})();
+	}, []);
+	return (
+		<div>
+			<h1>profile page</h1>
+			<div>
+				<p>{user.username}</p>
+				<p>{user.email}</p>
+				<p>{user.created}</p>
+				<p>{user.updated}</p>
+			</div>
+		</div>
+	);
 };
 
 export default Profile;
