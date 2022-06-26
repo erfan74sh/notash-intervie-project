46a22afa441b885e89891de7d27476a4296cecca
erfan shafiee <erfan74sh@gmail.com>
2022-06-27 03:35:11 +0430

    show logOut and username when user is logged in in Header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,19 @@
-import { faCircleNotch } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 import { NavLink, useNavigate } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// context
+import { authContext } from "../../Providers/AuthProvider";
+// services
 import AuthService from "../../services/auth.service";
+// icons
+import { faCircleNotch } from "@fortawesome/free-solid-svg-icons";
+// styles
 import style from "./style.module.scss";
 
 const Header = () => {
 	const navigate = useNavigate();
+
+	const { user, setUser } = useContext(authContext);
 
 	const [isLoading, setIsLoading] = useState(false);
 
@@ -20,6 +27,7 @@ const Header = () => {
 			console.log(err);
 		} finally {
 			setIsLoading(false);
+			setUser({});
 		}
 	};
 
@@ -40,21 +48,29 @@ const Header = () => {
 					</li>
 				</ul>
 				<ul className={style.nav__links}>
-					<li className={`${style.link} btn btn--secondary`}>
-						<NavLink to="/login">login</NavLink>
-					</li>
-					<li className={`${style.link} btn btn--primary`}>
-						<NavLink to="/register">register</NavLink>
-					</li>
-					<li className={`${style.link} btn btn--secondary`}>
-						<button onClick={handleLogout}>
-							{isLoading ? (
-								<FontAwesomeIcon icon={faCircleNotch} spin />
-							) : (
-								" logout"
-							)}
-						</button>
-					</li>
+					{user.email ? (
+						<>
+							<li>{`welcome ${user.username}`}</li>
+							<li className={`${style.link} btn btn--secondary`}>
+								<button onClick={handleLogout}>
+									{isLoading ? (
+										<FontAwesomeIcon icon={faCircleNotch} spin />
+									) : (
+										" logout"
+									)}
+								</button>
+							</li>
+						</>
+					) : (
+						<>
+							<li className={`${style.link} btn btn--secondary`}>
+								<NavLink to="/login">login</NavLink>
+							</li>
+							<li className={`${style.link} btn btn--primary`}>
+								<NavLink to="/register">register</NavLink>
+							</li>
+						</>
+					)}
 				</ul>
 			</nav>
 		</header>
