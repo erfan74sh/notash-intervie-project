f73322f7c3e6c8cb67cca9294555361d6f8066f8
erfan shafiee <erfan74sh@gmail.com>
2022-06-27 01:25:52 +0430

    change content of logout btn to spinner till server response on btn click

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,4 +1,6 @@
-import React from "react";
+import { faCircleNotch } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React, { useState } from "react";
 import { NavLink, useNavigate } from "react-router-dom";
 import AuthService from "../../services/auth.service";
 import style from "./style.module.scss";
@@ -6,13 +8,18 @@ import style from "./style.module.scss";
 const Header = () => {
 	const navigate = useNavigate();
 
+	const [isLoading, setIsLoading] = useState(false);
+
 	const handleLogout = async (e) => {
 		e.preventDefault();
+		setIsLoading(true);
 		try {
 			await AuthService.logout();
 			navigate("/login");
 		} catch (err) {
 			console.log(err);
+		} finally {
+			setIsLoading(false);
 		}
 	};
 
@@ -40,7 +47,13 @@ const Header = () => {
 						<NavLink to="/register">register</NavLink>
 					</li>
 					<li className={`${style.link} btn btn--secondary`}>
-						<button onClick={handleLogout}>log out</button>
+						<button onClick={handleLogout}>
+							{isLoading ? (
+								<FontAwesomeIcon icon={faCircleNotch} spin />
+							) : (
+								" logout"
+							)}
+						</button>
 					</li>
 				</ul>
 			</nav>
