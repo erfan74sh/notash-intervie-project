16ac138728df17d3b494bf5b8f93c65cb2dd9251
erfan shafiee <erfan74sh@gmail.com>
2022-06-27 01:20:29 +0430

    change context value on user login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,17 +1,25 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { authContext } from "../Providers/AuthProvider";
 import AuthService from "../services/auth.service";
+import UserService from "../services/user.service";
 
 const Login = () => {
-	const [user, setUser] = useState({ email: "", password: "" });
+	const [loginForm, setLoginForm] = useState({ email: "", password: "" });
+
+	const { setUser } = useContext(authContext);
 
 	const navigate = useNavigate();
 
 	const handleSubmit = async (e) => {
 		e.preventDefault();
 		try {
-			const session = await AuthService.login(user);
+			const session = await AuthService.login(loginForm);
 			if (session && session.id) {
+				const { account } = await UserService.getCurrentUser();
+				const { username, email, created, updated } = account;
+				console.log({ username, email, created, updated });
+				setUser({ username, email, created, updated });
 				navigate("/profile");
 			}
 		} catch (err) {
@@ -20,27 +28,27 @@ const Login = () => {
 	};
 
 	const handleFormChange = (e) => {
-		setUser({ ...user, [e.target.name]: e.target.value });
+		setLoginForm({ ...loginForm, [e.target.name]: e.target.value });
 	};
 	return (
-		<div>
+		<div className="content">
 			<h1>login page</h1>
 			<form onSubmit={handleSubmit}>
 				<label>email</label>
 				<input
 					type="email"
 					name="email"
-					value={user.email}
+					value={loginForm.email}
 					onChange={(e) => handleFormChange(e)}
 				/>
 				<label>password</label>
 				<input
 					type="password"
 					name="password"
-					value={user.password}
+					value={loginForm.password}
 					onChange={(e) => handleFormChange(e)}
 				/>
-				<button type="onSubmit">register</button>
+				<button type="onSubmit">login</button>
 			</form>
 		</div>
 	);
