9659beacaa96fbba45b1b4698ca2719460f7030f
erfan shafiee <erfan74sh@gmail.com>
2022-06-26 21:38:44 +0430

    add logout service

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -13,16 +13,27 @@ const login = ({ email, password }) => {
 		.then((response) => {
 			const session = response.data?.session;
 			if (session && session.id) {
-				localStorage.setItem("seassion", JSON.stringify(session));
+				localStorage.setItem("session", JSON.stringify(session));
 			}
-			return response.data;
+			return session;
 		})
 		.catch((err) => console.error("errrr", err));
+};
+
+const logout = () => {
+	const { id } = JSON.parse(localStorage.getItem("session"));
+	return Axios.post("Logout", { sessionId: id })
+		.then((response) => {
+			localStorage.removeItem("session");
+			return response.data;
+		})
+		.catch((err) => console.log(err));
 };
 
 const AuthService = {
 	register,
 	login,
+	logout,
 };
 
 export default AuthService;
