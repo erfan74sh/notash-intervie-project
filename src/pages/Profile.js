1e5a2c4e684b34a2b5a4073190a3cbb543d76f98
erfan shafiee <erfan74sh@gmail.com>
2022-06-27 18:09:44 +0430

    show current time in profile page and style elements

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,35 +1,39 @@
-import React, { useEffect, useState } from "react";
-import UserService from "../services/user.service";
+import React, { useContext, useEffect, useState } from "react";
+// context
+import { authContext } from "../Providers/AuthProvider";
+// style
+import style from "./profile.module.scss";
 
 const Profile = () => {
-	const [user, setUser] = useState({
-		username: "",
-		email: "",
-		created: "",
-		updated: "",
-	});
+	const { user } = useContext(authContext);
+
+	const [currentTime, setCurrentTime] = useState(
+		new Date().toLocaleTimeString()
+	);
+
 	useEffect(() => {
-		(async () => {
-			try {
-				const response = await UserService.getCurrentUser();
-				if (response.data && response.data.account) {
-					const { username, email, created, updated } = response.data.account;
-					setUser({ username, email, created, updated });
-				}
-			} catch (err) {
-				console.log(err);
-			}
-		})();
+		setInterval(() => setCurrentTime(new Date().toLocaleTimeString(), 1000));
 	}, []);
+
 	return (
-		<div>
-			<h1>profile page</h1>
-			<div>
-				<p>{user.username}</p>
-				<p>{user.email}</p>
-				<p>{user.created}</p>
-				<p>{user.updated}</p>
-			</div>
+		<div className="content">
+			<section className={style.section}>
+				<h2 className={style.section__title}>profile info</h2>
+				<ul className={style.items}>
+					<li className={style.items__item}>
+						<span className={style.item__title}>username:</span>
+						<span className={style.item__text}>{user.username}</span>
+					</li>
+					<li className={style.items__item}>
+						<span className={style.item__title}>email:</span>
+						<span className={style.item__text}>{user.email}</span>
+					</li>
+				</ul>
+			</section>
+			<section className={style.section}>
+				<h2 className={style.section__title}>current time</h2>
+				<span className={style.time}>{currentTime}</span>
+			</section>
 		</div>
 	);
 };
