ea563b2054adff70870e0ee32e123740c2328c18
erfan shafiee <erfan74sh@gmail.com>
2022-06-27 19:34:51 +0430

    show tasks in home page and style elements

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,43 @@
 import React from "react";
-import Header from "../components/Header/Header";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// icons
+import {
+	faCircleCheck,
+	faCircleXmark,
+} from "@fortawesome/free-solid-svg-icons";
+// constants
+import { tasks } from "../data/constants";
+// style
+import style from "./style.module.scss";
 
 const Home = () => {
 	return (
-		<div>
-			<Header />
+		<div className="content">
+			<h2 className={style.title}>Tasks</h2>
+			<ul className={style.items}>
+				{tasks.map((task) => (
+					<li className={style.item}>
+						<div className={style.item__header}>
+							<span
+								className={`${style.item__header__status} ${
+									task.status === "done"
+										? style["status--done"]
+										: style["status--not-done"]
+								}`}
+							>
+								<FontAwesomeIcon
+									icon={task.status === "done" ? faCircleCheck : faCircleXmark}
+									size="lg"
+								/>
+							</span>
+							<h3 className={style.item__header__title}>{task.title}</h3>
+						</div>
+						<p className={style.item__description}>
+							{task.processDesscription}
+						</p>
+					</li>
+				))}
+			</ul>
 		</div>
 	);
 };
